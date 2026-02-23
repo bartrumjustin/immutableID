4a52f21705e10a811c4cac9641154cf46583ee5c
bartrumjustin <bartrumjustin@gmail.com>
2026-02-23 13:20:51 -0500

    changed initial Student class to record so ToString method is auto, immutable the ID, 2nd param to set param and blank the strings, then set 2nd param and first param and display.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace immutableID
 {
+    public record class Student
+    {
+        // auto-implemented properties
+        public int Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // default constructor
+        public Student()
+        {
+            Id = 0;
+            FirstName = "";
+            LastName = "";
+        }
+        // parameterized constructor
+        public Student(int i, string First, string Last)
+        {
+            Id = i;
+            FirstName = First;
+            LastName = Last;
+        }
+        //2nd param const with Id only
+        public Student(int i)
+        {
+            Id = i;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student param = new(001);
+            param.FirstName = "joe";
+            param.LastName = "watson";
+            Console.WriteLine(param);
+            Student param2 = new(002, "john", "doe");
+            Console.WriteLine(param2);
         }
     }
 }
