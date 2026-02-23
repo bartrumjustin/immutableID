3c07e8ddbb6d416956e1a8192eb4e9581cc1654a
bartrumjustin <bartrumjustin@gmail.com>
2026-02-22 21:33:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace immutableID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
